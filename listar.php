061fef8d1cb03e915a131b214104c9d925dc54a8
ErickCode <54459575+ErickCode@users.noreply.github.com>
2021-10-23 16:41:24 -0500

    Add files via upload

--- listar.php
+++ listar.php
@@ -4,5 +4,15 @@
 
 	$query = "SELECT * FROM usuario WHERE estado = 1 ORDER BY idusuario desc;";
 	$resultado = mysqli_query($conexion, $query);
+	if (!$resultado){
+		die ("Error");
+	} else {
+		while( $data = mysqli_fetch_assoc($resultado)){
+			$arreglo["data"][] = $data;
+		}
+		echo json_encode($arreglo);
+	}
+	mysqli_free_results($resultado);
+	mysqli_close($conexion);
 
 	
